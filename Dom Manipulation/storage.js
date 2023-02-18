397e10b2d7d00cde7a903707bc46ddb0cccea191
Yaambs18 <yanshbhardwaj14@gmail.com>
2023-02-18 21:11:13 +0530

    added delte btn functionality to del elem from dom and local storage

--- Dom Manipulation/storage.js
+++ Dom Manipulation/storage.js
@@ -7,20 +7,28 @@ const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 
 // retrieving stored usersDetails from local storage
-var totalUsers = localStorage.getItem('usersCount');
+// var totalUsers = localStorage.getItem('usersCount');
 var usersCount = 0;
-if(totalUsers!=null){
-    usersCount = totalUsers;
-    let i=1;
-    while(i<=totalUsers){
-        const li = document.createElement('li');
-        let userObj_deserialized = JSON.parse(localStorage.getItem('user'+i))
-        var userDetails = document.createTextNode(`${userObj_deserialized.name} : ${userObj_deserialized.email}`);
-        li.appendChild(userDetails);
-        userList.appendChild(li);
-        i++;
-    }
-}
+// if(totalUsers!=null){
+//     usersCount = totalUsers;
+//     let i=1;
+//     while(i<=totalUsers){
+//         const li = document.createElement('li');
+//         let userObj_deserialized = JSON.parse(localStorage.getItem('user'+i))
+//         var userDetails = document.createTextNode(`${userObj_deserialized.name} : ${userObj_deserialized.email}`);
+//         li.appendChild(userDetails);
+
+//         // create delete btn element
+//         var delBtn = document.createElement('button');
+//         delBtn.className = 'delete';
+//         var delText = document.createTextNode('Delete');
+//         console.log(delBtn.appendChild(delText));
+//         li.appendChild(delBtn);
+//         userList.appendChild(li);
+//         i++;
+//     }
+// }
+
 myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e){
@@ -32,24 +40,46 @@ function onSubmit(e){
         setTimeout(() => msg.remove(), 3000);
     }
     else{
-        usersCount++;
-        console.log(nameInput.value);
-        console.log(emailInput.value);
+        // usersCount++;
         const li = document.createElement('li');
         var userDetails = document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
         li.appendChild(userDetails);
+
+        // create delete btn element
+        var delBtn = document.createElement('button');
+        delBtn.className = 'delete';
+        var delText = document.createTextNode('Delete');
+        delBtn.appendChild(delText);
+        li.appendChild(delBtn);
         userList.appendChild(li);
 
         // adding userdetails to local storage
-        localStorage.setItem('usersCount', usersCount);
+        // localStorage.setItem('usersCount', usersCount);
         let userObj = {
             name : nameInput.value,
             email : emailInput.value
         }
         let userObj_serialized = JSON.stringify(userObj);
-        localStorage.setItem('user'+usersCount, userObj_serialized);
+        localStorage.setItem(emailInput.value, userObj_serialized);
 
         nameInput.value = '';
         emailInput.value = '';
+    }
+}
+
+
+// delete event
+userList.addEventListener('click', deleteItem);
+
+// Remove item
+function deleteItem(e){
+
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you sure ?')){
+            var li = e.target.parentElement;
+            userList.removeChild(li);
+            let userEmail = li.firstChild.textContent.split(' : ')[1];
+            localStorage.removeItem(userEmail);
+        }
     }
 }
