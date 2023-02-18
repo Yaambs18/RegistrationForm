8e6443d5497dcdd6d8b1fd119c349867badd7ec0
Yaambs18 <yanshbhardwaj14@gmail.com>
2023-02-18 13:34:35 +0530

    adde filter option in the project to search item by name or desc

--- Dom Manipulation/project2/main.js
+++ Dom Manipulation/project2/main.js
@@ -1,5 +1,6 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 // form submit event
 form.addEventListener('submit', addItem);
@@ -7,18 +8,25 @@ form.addEventListener('submit', addItem);
 // delete event
 itemList.addEventListener('click', deleteItem);
 
+// filter event
+filter.addEventListener('keyup', filterItems);
+
+
 // Add item 
 function addItem(e){
     e.preventDefault();
 
     // Get Input
     var newItem = document.getElementById('item').value;
+    var newItemDesc = document.getElementById('description').value;
 
     // createElement
     var newLi = document.createElement('li');
     newLi.className = 'list-group-item';
     // Add text node with input value
     newLi.appendChild(document.createTextNode(newItem));
+    // Add desc node with input value
+    newLi.appendChild(document.createTextNode(' '+newItemDesc));
 
     // create edit button element
     var editBtn = document.createElement('button');
@@ -51,4 +59,22 @@ function deleteItem(e){
             itemList.removeChild(li);
         }
     }
+}
+
+// filter items
+function filterItems(e){
+    // convert to lower case
+    var text = e.target.value.toLowerCase();
+    // get list
+    var items = itemList.getElementsByTagName('li');
+    // convert to an array
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        var itemDesc = item.childNodes[1].textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1 || itemDesc.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        }else{
+            item.style.display = 'none';
+        }
+    })
 }
