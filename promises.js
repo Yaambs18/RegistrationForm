7844b97013d0789eac6a75ca2e0b73711c81c019
Yaambs18 <yanshbhardwaj14@gmail.com>
2023-02-25 15:16:34 +0530

    implemented promises.then into async await

--- promises.js
+++ promises.js
@@ -1,44 +1,86 @@
-let userActivityTime = new Date();
-const posts = [{title: 'POST1'}, {title: 'POST2'}, {title: 'POST3'}];
+// let userActivityTime = new Date();
+// const posts = [{title: 'POST1'}, {title: 'POST2'}, {title: 'POST3'}];
 
-function updateLastUserActivity(){
-    return new Promise((resolve, reject) => {
+// function updateLastUserActivity(){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() =>{
+//             userActivityTime = new Date();
+//             resolve(userActivityTime);
+//         }, 1000);
+//     })
+// }
+
+// function printPost() {
+//     return new Promise((resolve, reject) => {
+//         posts.forEach((post) => {
+//             console.log(post.title)
+//         })
+//         resolve();
+//     })
+// }
+
+// function createPost() {
+//     return new Promise( (resolve, reject) => {
+//         console.log(`User last activity time before creating post 4 ${userActivityTime}`);
+//         posts.push({title: 'POST4'});
+//         resolve();
+//     }) 
+// }
+
+// function deletePost(){
+//     return new Promise((resolve, reject) => {
+//         posts.pop();
+//         resolve();
+//     })
+// }
+
+
+// Promise.all([createPost(), updateLastUserActivity()]).then(([createdPostData, updatedTime]) => {
+//     printPost();
+//     console.log(updatedTime);
+//     deletePost().then(() =>{
+//         printPost();
+//     })
+// })
+
+
+let prePost = async () => {
+    let userActivityTime = new Date();
+    const posts = [{title: 'POST1'}, {title: 'POST2'}, {title: 'POST3'}];
+
+    const post = new Promise( (resolve, reject) => {
+        console.log(`User last activity time before creating post 4 ${userActivityTime}`);
+        posts.push({title: 'POST4'});
+        resolve();
+    });
+    
+    const updatedTime = new Promise((resolve, reject) => {
         setTimeout(() =>{
             userActivityTime = new Date();
             resolve(userActivityTime);
         }, 1000);
-    })
-}
-
-function printPost() {
-    return new Promise((resolve, reject) => {
+    });
+    
+    let printPost = () => {
         posts.forEach((post) => {
             console.log(post.title)
         })
-        resolve();
-    })
-}
+    }
 
-function createPost() {
-    return new Promise( (resolve, reject) => {
-        console.log(`User last activity time before creating post 4 ${userActivityTime}`);
-        posts.push({title: 'POST4'});
-        resolve();
-    }) 
-}
 
-function deletePost(){
-    return new Promise((resolve, reject) => {
+    const [allPosts, lastActiveTime] = await Promise.all([post, updatedTime]);
+
+    printPost();
+    console.log(lastActiveTime);
+
+    let deletePost = new Promise((resolve, reject) => {
         posts.pop();
         resolve();
-    })
+    });
+
+    let deleted = await deletePost;
+
+    printPost();
 }
 
-
-Promise.all([createPost(), updateLastUserActivity()]).then(([createdPostData, updatedTime]) => {
-    printPost();
-    console.log(updatedTime);
-    deletePost().then(() =>{
-        printPost();
-    })
-})
+prePost();
